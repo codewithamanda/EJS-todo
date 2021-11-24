9b04a7b018fbf2094bd539a9d67b461b22e192b2
Amanda1024 <88503704+codewithamanda@users.noreply.github.com>
2021-11-24 13:15:59 -0800

    Add date module, create header and footer, refactor code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date =require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems =[];
+const items = [];
+const workItems =[];
 
 app.set("view engine", "ejs");
 
@@ -12,17 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    
-    let today = new Date();
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-
-    let day = today.toLocaleDateString("en-us", options);
-
+    let day = date.getDate();
 
     res.render("list", {listTitle: day, newListItems: items})
 });
