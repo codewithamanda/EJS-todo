851fa75b3b62354882d8cf5f257d4851132d393c
Amanda1024 <88503704+codewithamanda@users.noreply.github.com>
2021-12-01 10:55:20 -0800

    Configure app to deploy on Heroku

--- app.js
+++ app.js
@@ -132,6 +132,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(6060, function() {
-  console.log("Server started on port 6060");
+let port = process.env.PORT;
+if (port === null || port == "") {
+    port = 6060;
+}
+
+
+app.listen(port, function() {
+  console.log("Server started");
 });
